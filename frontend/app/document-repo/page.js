a7a97699aa5493610c2218e51411c8be2ea6e138
ngao8082 <danielngao1999@gmail.com>
2024-02-21 00:39:06 +0300

    merging master with daniel

--- frontend/app/document-repo/page.js
+++ frontend/app/document-repo/page.js
@@ -96,7 +96,7 @@ export default function DocumentRepo() {
                   
               </div>
             </div>
-          ))}
+          ))&rbrace;
         </div>
     </div>
     </Drawer>
