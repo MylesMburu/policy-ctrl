8ab9859d4f2df063a60852e60db918faff8ed7a4
MylesMburu <ngicumyles@gmail.com>
2024-04-11 12:25:10 +0300

    added blog page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,11 +4,13 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FiMenu, FiX } from 'react-icons/fi';
+import { RiArrowDownSFill } from "react-icons/ri";
 
 import logo1 from '@/public/logo1.svg';
 
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <div>
@@ -32,10 +34,32 @@ export const Nav = () => {
 
           {/* Navigation Links */}
           <ul className={`md:flex ${isOpen ? 'flex flex-col mr-24 gap-2 ' : 'hidden'} md:flex-row gap-4`}>
-            <li><Link href="/about"><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>About Us</span></Link></li>
-            <li><Link href="/features"><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>Features</span></Link></li>
+          <li className="relative">
+      <span 
+        className="hover:text-blue-500 cursor-pointer flex"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        About Us <RiArrowDownSFill className='mt-1 ml-1'/>
+      </span>
+      
+      {isDropdownOpen && (
+        <div className="absolute mt-1 bg-white shadow-lg rounded min-w-max">
+          <ul>
+            <li>
+              <Link href="/about/history" className="block px-4 py-2 hover:bg-gray-100">Our History
+              </Link>
+            </li>
+            <li>
+              <Link href="/about/team" className="block px-4 py-2 hover:bg-gray-100">Our Team
+              </Link>
+            </li>
+            {/* Add more dropdown items here */}
+          </ul>
+        </div>
+      )}
+    </li>
             <li><Link href="/pricing"><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>Pricing</span></Link></li>
-            <li><Link href="/blog"><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>Blog</span></Link></li>
+            <li><Link href="https://medium.com/@policyctrl" target='_blank'><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>Blog</span></Link></li>
             <li><Link href="/contact"><span className="hover:text-blue-500" onClick={() => setIsOpen(false)}>Contact Us</span></Link></li>
           </ul>
         </div>
