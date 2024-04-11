df5b574a5ecb30bb0e34ee280748b8feffc284fb
MylesMburu <ngicumyles@gmail.com>
2024-04-11 13:35:39 +0300

    still metatags

--- app/layout.js
+++ app/layout.js
@@ -5,33 +5,26 @@ import logo from '@/public/logo.svg'
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Policy CTRL",
+  description: "Empowering Climate Action through Data-Driven Policy Solutions",
+  
+  metadataBase: new URL('https://policy-ctrl.vercel.app/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    images: {logo},
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <>
-      <Head>
-        {/* Primary Meta Tags */}
-        <title>POLICY CTRL</title>
-        <meta name="description" content="Empowering Climate Action through Data-Driven Policy Solutions. Policy Ctrl is a groundbreaking platform that bridges the gap between climate policies and technology." />
-
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://policy-ctrl.vercel.app/" />
-        <meta property="og:title" content="POLICY CTRL" />
-        <meta property="og:description" content="Empowering Climate Action through Data-Driven Policy Solutions. Policy Ctrl is a groundbreaking platform that bridges the gap between climate policies and technology." />
-        <meta property="og:image" content={logo} />
-
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://policy-ctrl.vercel.app/" />
-        <meta property="twitter:title" content="POLICY CTRL" />
-        <meta property="twitter:description" content="Empowering Climate Action through Data-Driven Policy Solutions. Policy Ctrl is a groundbreaking platform that bridges the gap between climate policies and technology." />
-        <meta property="twitter:image" content={logo} />
-
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        {/* Add your Google font link here */}
-      </Head>
+      
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
